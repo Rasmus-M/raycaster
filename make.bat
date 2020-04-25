4736f077a500e6663671436bfa7128dfbb6ebd3a
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2020-04-25 16:32:48 +0200

    Make rpk

--- make.bat
+++ make.bat
@@ -5,7 +5,7 @@ xas99.py -R -i -w src/raycaster.a99 -o bin/RAYCASTER
 
 java -jar tools/ea5tocart.jar bin/RAYCASTER "RAYCASTER" > make.log
 
-copy bin\RAYCASTER8.bin .\raycaster3-8.bin
+copy bin\RAYCASTER8.bin .\raycaster8.bin
 
 IF EXIST raycaster.dsk GOTO :dskok
 xdm99.py raycaster.dsk --initialize DSSD -n RAYCASTER
@@ -13,8 +13,8 @@ xdm99.py raycaster.dsk --initialize DSSD -n RAYCASTER
 
 xdm99.py raycaster.dsk -a bin/RAYCASTER
 
-rem WHERE jar
-rem @IF %ERRORLEVEL% NEQ 0 GOTO :end
-rem jar -cvf tix.rpk tix8.bin layout.xml > make.log
+WHERE jar
+@IF %ERRORLEVEL% NEQ 0 GOTO :end
+jar -cvf raycaster.rpk raycaster8.bin layout.xml > make.log
 
 :end
