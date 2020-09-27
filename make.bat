a051b60f193fd4043aa3d6beef5ff2f7701f1278
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2020-09-27 22:24:14 +0200

    32 width wall texture

--- make.bat
+++ make.bat
@@ -8,11 +8,16 @@ java -jar tools/ea5tocart.jar bin/RAYCASTER "TEXCASTER" > make.log
 xas99.py -b -w textures/texture-bank-0.a99
 xas99.py -b -w textures/texture-bank-1.a99
 xas99.py -b -w textures/texture-bank-2.a99
+xas99.py -b -w textures/texture-bank-3.a99
+xas99.py -b -w textures/texture-bank-4.a99
+xas99.py -b -w textures/texture-bank-5.a99
+xas99.py -b -w textures/texture-bank-6.a99
+xas99.py -b -w textures/texture-bank-7.a99
 
 copy /b bin\RAYCASTER8.bin + ^
-    texture-bank-0.bin + ^
-    texture-bank-1.bin + ^
-    texture-bank-2.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
     TexGen\col-bank7.bin + ^
     TexGen\col-bank8.bin + ^
     TexGen\col-bank9.bin + ^
@@ -37,10 +42,42 @@ copy /b bin\RAYCASTER8.bin + ^
     bin\empty-2k.bin + backgrounds\background4odd.bin + ^
     bin\empty-2k.bin + backgrounds\background5odd.bin + ^
     bin\empty-2k.bin + backgrounds\background6odd.bin + ^
-    bin\empty-2k.bin + backgrounds\background7odd.bin ^
+    bin\empty-2k.bin + backgrounds\background7odd.bin + ^
+    texture-bank-0.bin + ^
+    texture-bank-1.bin + ^
+    texture-bank-2.bin + ^
+    texture-bank-3.bin + ^
+    texture-bank-4.bin + ^
+    texture-bank-5.bin + ^
+    texture-bank-6.bin + ^
+    texture-bank-7.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin + ^
+    bin\empty.bin ^
     .\texcaster8.bin
 
-java -jar tools/CopyHeader.jar texcaster8.bin 60 4 5 6 7 8 9 10 11 12 13 14 15
+java -jar tools/CopyHeader.jar texcaster8.bin 60 7 8 9 10 11 12 13 14 15 32 33 34 35 36 37 38 39
 
 WHERE jar
 @IF %ERRORLEVEL% NEQ 0 GOTO :end
