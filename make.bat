aaa5804c22c23972feb98d7ab2bdde50aec93bdf
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2020-10-09 18:19:48 +0200

    Added lots of monster textures

--- make.bat
+++ make.bat
@@ -50,22 +50,22 @@ cd ..
     bin\texture-bank-5.bin + ^
     bin\texture-bank-6.bin + ^
     bin\texture-bank-7.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
-    bin\empty.bin + ^
+    bin\texture-bank-8.bin + ^
+    bin\texture-bank-9.bin + ^
+    bin\texture-bank-10.bin + ^
+    bin\texture-bank-11.bin + ^
+    bin\texture-bank-12.bin + ^
+    bin\texture-bank-13.bin + ^
+    bin\texture-bank-14.bin + ^
+    bin\texture-bank-15.bin + ^
+    bin\texture-bank-16.bin + ^
+    bin\texture-bank-17.bin + ^
+    bin\texture-bank-18.bin + ^
+    bin\texture-bank-19.bin + ^
+    bin\texture-bank-20.bin + ^
+    bin\texture-bank-21.bin + ^
+    bin\texture-bank-22.bin + ^
+    bin\texture-bank-23.bin + ^
     bin\empty.bin + ^
     bin\empty.bin + ^
     bin\empty.bin + ^
@@ -76,7 +76,7 @@ cd ..
     bin\empty.bin ^
     .\texcaster8.bin
 
-java -jar tools/CopyHeader.jar texcaster8.bin 60 7 8 9 10 11 12 13 14 15 32 33 34 35 36 37 38 39
+java -jar tools/CopyHeader.jar texcaster8.bin 60 7 8 9 10 11 12 13 14 15 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55
 
 WHERE jar
 @IF %ERRORLEVEL% NEQ 0 GOTO :end
