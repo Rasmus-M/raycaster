2638865279a85d6466fb6e22146d114f2b5318bb
Rasmus Moustgaard <rmoustgaard@cochrane.org>
2021-01-12 20:56:55 +0100

    Half size sprites

--- LevelMap/src/Sprite.java
+++ LevelMap/src/Sprite.java
@@ -7,8 +7,8 @@ public class Sprite {
             "sprite_type_pillar_broken",
             "sprite_type_grate",
             "sprite_type_stump",
-            "sprite_type_obj6",
-            "sprite_type_obj7",
+            "sprite_type_red_potion",
+            "sprite_type_green_potion",
             // Monsters
             "sprite_type_eye",
             "sprite_type_blob",
